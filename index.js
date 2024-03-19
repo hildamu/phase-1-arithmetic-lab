359c2c43aa8191b9194c7583d8ebc113417190ae
hildamu <mukamihilda437@gmail.com>
2024-03-19 14:05:48 +0300

    include new files

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2 
+let random = Math.floor(Math.random()+1)
+let num3 = 25
+let num4 = 7
+let mod = 25 % 7
+const num = (5,10,15,20);
+const max = Math.max(num)
+let infinity = 20
+console.log(infinity)
